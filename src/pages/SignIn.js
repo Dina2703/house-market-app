575565b0a3fec7c0af87a9766dc44bbbf3b95be3
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-05-25 14:20:42 -0400

    Google OAuth

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -4,6 +4,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 import { toast } from "react-toastify";
+import OAuth from "../components/OAuth";
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -78,6 +79,7 @@ function SignIn() {
           </Link>
           <div className="signInBar">
             <p className="signInText">Sign In</p>
+
             <button className="signInButton">
               <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
             </button>
@@ -85,6 +87,7 @@ function SignIn() {
         </form>
 
         {/*Google OAuth */}
+        <OAuth />
         <Link to="/sign-up" className="registerLink">
           Sign Up Instead
         </Link>
