7afb9594b4c400c92f812498d4a095b926d2fc10
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-06-02 11:50:38 -0400

    Explore Slider

---
+++ src/components/Slider.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { db } from "../firebase.config";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import Spinner from "./Spinner";
+
+//To enable the modules, so you can see navigation on the slider, as white dots
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+function Slider() {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingRef = collection(db, "listings");
+      const q = query(listingRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnapShot = await getDocs(q);
+
+      let listings = [];
+
+      querySnapShot.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      console.log(listings);
+      setListings(listings);
+      setLoading(false);
+    };
+    fetchListings();
+  }, []);
+
+  if (loading) {
+    <Spinner />;
+  }
+  return (
+    listings && (
+      <>
+        <p className="exploreHeading">Recommended</p>
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {/* restuctur  listing  obj, and get data, id from it */}
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                className="swiperSlideDiv"
+                style={{
+                  background: `url(${data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              >
+                <p className="swiperSlideText">{data.name}</p>
+                <p className="swiperSlidePrice">
+                  {/* ?? - nullish coalescing operator, It's an operator that simply returns the right-side expression when the left side expression is either null or undefined */}
+                  $ {data.discountedPrice ?? data.regularPrice}{" "}
+                  {data.type === "rent" && "/ month"}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+}
+
+export default Slider;
